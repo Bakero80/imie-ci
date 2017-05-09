640b155360de28a363df6a9fc3adc8385a3fb08e
bakero80 <bakero82@live.fr>
2017-05-09 16:39:57 +0200

    hop

--- test/FinderTest.php
+++ test/FinderTest.php
@@ -11,4 +11,10 @@ class FinderTest extends \PHPUnit_Framework_TestCase
     public function testOk() {
         $this->assertTrue(true);
     }
+
+    public function testEssai(){
+        $a = new Finder('banane',['Kiwi', 'banane']);
+        // echo $a->first();
+        $this->assertEquals('Ok', $a->first());
+    }
 }
